a2e1843d365b0a1241e3da52325b8679eb63f7a0
nmanerikar <nmanerikar@yahoo.com>
2024-09-30 21:33:13 +0200

    Add support for click-to-move; backgroundColor and disable/enable dragging

--- src/chessboard/components/SparePiece.tsx
+++ src/chessboard/components/SparePiece.tsx
@@ -1,58 +1,72 @@
-import { ReactNode } from "react";
-import { useDrag } from "react-dnd";
-import { getEmptyImage } from "react-dnd-html5-backend";
+import { ReactNode } from 'react';
+import { useDrag } from 'react-dnd';
+import { getEmptyImage } from 'react-dnd-html5-backend';
 
-import { CustomPieceFn, Piece as Pc } from "../types";
-import { defaultPieces } from "../media/pieces";
+import { CustomPieceFn, Piece as Pc } from '../types';
+import { defaultPieces } from '../media/pieces';
 
 type PieceProps = {
-  piece: Pc;
-  width: number;
-  customPieceJSX?: CustomPieceFn;
-  dndId: string;
+    piece: Pc;
+    width: number;
+    customPieceJSX?: CustomPieceFn;
+    dndId: string;
+    onClick?: (piece: Pc) => void;
+    backgroundColor?: string;
+    draggable?: boolean;
 };
 
 export const SparePiece = ({
-  piece,
-  width,
-  customPieceJSX,
-  dndId,
+    piece,
+    width,
+    customPieceJSX,
+    dndId,
+    onClick,
+    backgroundColor,
+    draggable = true,
 }: PieceProps) => {
-  const renderPiece = customPieceJSX ?? defaultPieces[piece];
-  const [{ canDrag, isDragging }, drag, dragPreview] = useDrag(
-    () => ({
-      type: "piece",
-      item: () => {
-        return { piece, isSpare: true, id: dndId };
-      },
+    const renderPiece = customPieceJSX ?? defaultPieces[piece];
+    const [{ canDrag, isDragging }, drag, dragPreview] = useDrag(
+        () => ({
+            type: 'piece',
+            item: () => {
+                return { piece, isSpare: true, id: dndId };
+            },
 
-      collect: (monitor) => ({
-        canDrag: true,
-        isDragging: !!monitor.isDragging(),
-      }),
-    }),
-    [piece, dndId]
-  );
+            collect: (monitor) => ({
+                canDrag: true,
+                isDragging: !!monitor.isDragging(),
+            }),
+        }),
+        [piece, dndId]
+    );
 
-  // hide the default preview
-  dragPreview(getEmptyImage(), { captureDraggingState: true });
+    // hide the default preview
+    dragPreview(getEmptyImage(), { captureDraggingState: true });
 
-  return (
-    <div
-      ref={canDrag ? drag : null}
-      data-piece={piece}
-      style={{ cursor: "move" }}
-    >
-      {typeof renderPiece === "function" ? (
-        (renderPiece as CustomPieceFn)({
-          squareWidth: width,
-          isDragging,
-        })
-      ) : (
-        <svg viewBox={"1 1 43 43"} width={width} height={width}>
-          <g>{renderPiece as ReactNode}</g>
-        </svg>
-      )}
-    </div>
-  );
+    return (
+        <div
+            ref={canDrag && draggable ? drag : null}
+            data-piece={piece}
+            style={{
+                cursor: draggable ? 'pointer' : 'default',
+                backgroundColor,
+            }}
+            onClick={() => {
+                if (onClick) {
+                    onClick(piece);
+                }
+            }}
+        >
+            {typeof renderPiece === 'function' ? (
+                (renderPiece as CustomPieceFn)({
+                    squareWidth: width,
+                    isDragging,
+                })
+            ) : (
+                <svg viewBox={'1 1 43 43'} width={width} height={width}>
+                    <g>{renderPiece as ReactNode}</g>
+                </svg>
+            )}
+        </div>
+    );
 };
